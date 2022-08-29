90d7b1a8d7753085cbf09c4e1a588117a6c8ba6e
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-08-29 15:30:51 -0300

    Cria migration de PostCategories

---
+++ src/database/migrations/20220829175800-create-posts-categories.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+   await queryInterface.createTable('PostCategories', {
+      postId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        references: {
+            model: 'BlogPosts',
+            key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+    },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        references: {
+          model: 'Categories',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+    },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostCategories');
+
+  }
+};
