b300afdb62a0cfb99324a70eac020d2a9b15abc5
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-09-02 14:49:47 -0300

    Cria BlogPost model

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,27 @@
+const BlogPost = (sequelize, DataTypes) => {
+    const BlogPost = sequelize.define('BlogPost', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      userId: {
+        foreignKey: true,
+        type: DataTypes.INTEGER,
+      },
+      published: DataTypes.DATE,
+      updated: DataTypes.DATE,
+      },
+    {
+        timestamps: false,});
+  
+    BlogPost.associate = (models) => {
+      BlogPost.belongsTo(models.User, {foreignKey: 'userId'})
+    }
+  
+    return BlogPost;
+  }
+  
+  module.exports = BlogPost;
